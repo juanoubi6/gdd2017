2662d6ff25fb5deab87f5720dcf5d9b72ea689c3
Juan Manuel Oubina <juan.manuel.oubina@gmail.com>
2017-04-22 18:20:26 -0300

    Agregado menu y login

---
+++ MenuPrincipal.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace UberFrba
+{
+    partial class MenuPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuOpciones = new System.Windows.Forms.MenuStrip();
+            this.aBMsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facturacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viajeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registroDeViajeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rendiconDeViajeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listadoEstadisticoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.facturacionToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.automovilToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.choferToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clienteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.rolToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.turnoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.salirToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listadoEstadisticoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuOpciones.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuOpciones
+            // 
+            this.menuOpciones.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aBMsToolStripMenuItem,
+            this.facturacionToolStripMenuItem,
+            this.viajeToolStripMenuItem,
+            this.listadoEstadisticoToolStripMenuItem,
+            this.salirToolStripMenuItem});
+            this.menuOpciones.Location = new System.Drawing.Point(0, 0);
+            this.menuOpciones.Name = "menuOpciones";
+            this.menuOpciones.Size = new System.Drawing.Size(373, 24);
+            this.menuOpciones.TabIndex = 0;
+            this.menuOpciones.Text = "menuStrip1";
+            // 
+            // aBMsToolStripMenuItem
+            // 
+            this.aBMsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.automovilToolStripMenuItem,
+            this.choferToolStripMenuItem,
+            this.clienteToolStripMenuItem,
+            this.rolToolStripMenuItem,
+            this.turnoToolStripMenuItem});
+            this.aBMsToolStripMenuItem.Name = "aBMsToolStripMenuItem";
+            this.aBMsToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.aBMsToolStripMenuItem.Text = "ABMs";
+            // 
+            // facturacionToolStripMenuItem
+            // 
+            this.facturacionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.facturacionToolStripMenuItem1});
+            this.facturacionToolStripMenuItem.Name = "facturacionToolStripMenuItem";
+            this.facturacionToolStripMenuItem.Size = new System.Drawing.Size(81, 20);
+            this.facturacionToolStripMenuItem.Text = "Facturacion";
+            // 
+            // viajeToolStripMenuItem
+            // 
+            this.viajeToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registroDeViajeToolStripMenuItem,
+            this.rendiconDeViajeToolStripMenuItem});
+            this.viajeToolStripMenuItem.Name = "viajeToolStripMenuItem";
+            this.viajeToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.viajeToolStripMenuItem.Text = "Viaje";
+            // 
+            // registroDeViajeToolStripMenuItem
+            // 
+            this.registroDeViajeToolStripMenuItem.Name = "registroDeViajeToolStripMenuItem";
+            this.registroDeViajeToolStripMenuItem.Size = new System.Drawing.Size(168, 22);
+            this.registroDeViajeToolStripMenuItem.Text = "Registro de Viaje";
+            this.registroDeViajeToolStripMenuItem.Click += new System.EventHandler(this.registroDeViajeToolStripMenuItem_Click);
+            // 
+            // rendiconDeViajeToolStripMenuItem
+            // 
+            this.rendiconDeViajeToolStripMenuItem.Name = "rendiconDeViajeToolStripMenuItem";
+            this.rendiconDeViajeToolStripMenuItem.Size = new System.Drawing.Size(171, 22);
+            this.rendiconDeViajeToolStripMenuItem.Text = "Rendicion de Viaje";
+            this.rendiconDeViajeToolStripMenuItem.Click += new System.EventHandler(this.rendiconDeViajeToolStripMenuItem_Click);
+            // 
+            // listadoEstadisticoToolStripMenuItem
+            // 
+            this.listadoEstadisticoToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listadoEstadisticoToolStripMenuItem1});
+            this.listadoEstadisticoToolStripMenuItem.Name = "listadoEstadisticoToolStripMenuItem";
+            this.listadoEstadisticoToolStripMenuItem.Size = new System.Drawing.Size(116, 20);
+            this.listadoEstadisticoToolStripMenuItem.Text = "Listado estadistico";
+            // 
+            // facturacionToolStripMenuItem1
+            // 
+            this.facturacionToolStripMenuItem1.Name = "facturacionToolStripMenuItem1";
+            this.facturacionToolStripMenuItem1.Size = new System.Drawing.Size(152, 22);
+            this.facturacionToolStripMenuItem1.Text = "Facturacion";
+            this.facturacionToolStripMenuItem1.Click += new System.EventHandler(this.facturacionToolStripMenuItem1_Click);
+            // 
+            // automovilToolStripMenuItem
+            // 
+            this.automovilToolStripMenuItem.Name = "automovilToolStripMenuItem";
+            this.automovilToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.automovilToolStripMenuItem.Text = "Automovil";
+            this.automovilToolStripMenuItem.Click += new System.EventHandler(this.automovilToolStripMenuItem_Click);
+            // 
+            // choferToolStripMenuItem
+            // 
+            this.choferToolStripMenuItem.Name = "choferToolStripMenuItem";
+            this.choferToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.choferToolStripMenuItem.Text = "Chofer";
+            this.choferToolStripMenuItem.Click += new System.EventHandler(this.choferToolStripMenuItem_Click);
+            // 
+            // clienteToolStripMenuItem
+            // 
+            this.clienteToolStripMenuItem.Name = "clienteToolStripMenuItem";
+            this.clienteToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.clienteToolStripMenuItem.Text = "Cliente";
+            this.clienteToolStripMenuItem.Click += new System.EventHandler(this.clienteToolStripMenuItem_Click);
+            // 
+            // rolToolStripMenuItem
+            // 
+            this.rolToolStripMenuItem.Name = "rolToolStripMenuItem";
+            this.rolToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.rolToolStripMenuItem.Text = "Rol";
+            this.rolToolStripMenuItem.Click += new System.EventHandler(this.rolToolStripMenuItem_Click);
+            // 
+            // turnoToolStripMenuItem
+            // 
+            this.turnoToolStripMenuItem.Name = "turnoToolStripMenuItem";
+            this.turnoToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.turnoToolStripMenuItem.Text = "Turno";
+            this.turnoToolStripMenuItem.Click += new System.EventHandler(this.turnoToolStripMenuItem_Click);
+            // 
+            // salirToolStripMenuItem
+            // 
+            this.salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            this.salirToolStripMenuItem.Size = new System.Drawing.Size(41, 20);
+            this.salirToolStripMenuItem.Text = "Salir";
+            this.salirToolStripMenuItem.Click += new System.EventHandler(this.salirToolStripMenuItem_Click);
+            // 
+            // listadoEstadisticoToolStripMenuItem1
+            // 
+            this.listadoEstadisticoToolStripMenuItem1.Name = "listadoEstadisticoToolStripMenuItem1";
+            this.listadoEstadisticoToolStripMenuItem1.Size = new System.Drawing.Size(171, 22);
+            this.listadoEstadisticoToolStripMenuItem1.Text = "Listado estadistico";
+            this.listadoEstadisticoToolStripMenuItem1.Click += new System.EventHandler(this.listadoEstadisticoToolStripMenuItem1_Click);
+            // 
+            // MenuPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(373, 280);
+            this.Controls.Add(this.menuOpciones);
+            this.MainMenuStrip = this.menuOpciones;
+            this.Name = "MenuPrincipal";
+            this.Text = "MenuPrincipal";
+            this.menuOpciones.ResumeLayout(false);
+            this.menuOpciones.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuOpciones;
+        private System.Windows.Forms.ToolStripMenuItem aBMsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem automovilToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem choferToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clienteToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rolToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem turnoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facturacionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem facturacionToolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem viajeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registroDeViajeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem rendiconDeViajeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listadoEstadisticoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem salirToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listadoEstadisticoToolStripMenuItem1;
+    }
+}
---
+++ MenuPrincipal.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UberFrba
+{
+    public partial class MenuPrincipal : Form
+    {
+        public MenuPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("Esta seguro que desea salir de la aplicación?", "Atención", MessageBoxButtons.YesNoCancel))
+            {
+                Application.Exit();
+            }
+        }
+
+        private void automovilToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void choferToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void clienteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rolToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void turnoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void facturacionToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void registroDeViajeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rendiconDeViajeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listadoEstadisticoToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
